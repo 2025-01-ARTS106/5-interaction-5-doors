f41caea76eaa82e4f3e7c74b8ee328fd8ef07d89
Breadbatter <Breadbatter35@gmail.com>
2025-02-19 11:55:42 -0500

    door collider

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start(Collider other)
     {
-        
+        transform.Translate((float)0.25,(float)-1.3, 0);
     }
 
     // Update is called once per frame
-    void Update()
+    void Update(Collider other)
     {
-        
+        transform.Translate((float)0.25, (float)-1.3, 0);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        transform.Translate((float)0.25, (float)1.789, 0);
     }
 }
