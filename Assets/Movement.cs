f4ce88f85e0e7bade888d16915c80dbcf0e210de
Breadbatter <Breadbatter35@gmail.com>
2025-02-19 12:30:30 -0500

    added script

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start(Collider other)
-    {
-        transform.Translate((float)0.25,(float)-1.3, 0);
-    }
+    private GameObject door;
 
-    // Update is called once per frame
-    void Update(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        transform.Translate((float)0.25, (float)-1.3, 0);
-    }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            door.transform.Translate(new Vector3((float)0.25, (float)-1.8, 0));
+        }
+
+
+}
     private void OnTriggerExit(Collider other)
     {
-        transform.Translate((float)0.25, (float)1.789, 0);
+        if (other.gameObject.CompareTag("Player"))
+            {
+            door.transform.Translate(new Vector3((float)0.25, (float)1.789, 0));
+        }
     }
 }
