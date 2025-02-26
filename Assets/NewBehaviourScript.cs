cfd06586afc7538b276d2f631c5e6acf66f5cc5a
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2025-02-26 12:31:51 -0500

    started codding
    Will need to look up on how to code.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle; 
+
+    private GameObject Door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
